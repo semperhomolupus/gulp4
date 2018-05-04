136ba4abe8ee59233962da08c00c3326df597619
Егор Дьяченко <egordacenko@Egors-Mac-mini.local>
2018-05-04 17:59:51 +0300

    Поправил работу спрайта

--- gulp/tasks/sprite.js
+++ gulp/tasks/sprite.js
@@ -1,14 +1,21 @@
 module.exports = function() {
   p.gulp.task("sprite", function() {
     // Спрайты из PNG
-    var data = p.gulp.src(p.paths.src.sprite).pipe(
-      p.spritesmith({
-        imgName: "sprite.png",
-        cssName: "_sprite.scss",
-        cssFormat: "scss",
-        padding: 20
-      })
-    );
+    var data = p.gulp
+      .src(p.paths.src.sprite)
+      .pipe(p.gp.cache(p.gp.tinypngNokey()))
+      .pipe(
+        p.spritesmith({
+          imgName: "sprite.png",
+          cssName: "_sprite.scss",
+          cssFormat: "scss",
+          imgPath: "../img/sprite/sprite.png",
+          padding: 20,
+          cssVarMap: function(sprite) {
+            sprite.name = "sprite-" + sprite.name; //им файла + конструкция 'sprite-' в начале имени
+          }
+        })
+      );
 
     var imgStream = data.img.pipe(p.gulp.dest(p.paths.build.spriteIMG));
     var cssStream = data.css.pipe(p.gulp.dest(p.paths.build.spriteCSS));
