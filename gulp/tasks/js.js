22bf3b44cc61f3752e80784d51eefc347fc01379
Егор Дьяченко <egordacenko@Egors-Mac-mini.local>
2018-05-11 18:11:29 +0300

    Добавил Babel

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,10 +1,10 @@
-module.exports = function() {
-  p.gulp.task("js", function() {
+module.exports = function () {
+  p.gulp.task("js", function () {
     return p.gulp
       .src(p.paths.src.js)
       .pipe(
         p.gp.plumber({
-          errorHandler: p.gp.notify.onError(function(error) {
+          errorHandler: p.gp.notify.onError(function (error) {
             return {
               title: "JavaScript - ошибка при сборке .js",
               message: error.message
@@ -13,11 +13,18 @@ module.exports = function() {
         })
       ) // Предотвращает остановку плагина при возникновении ошибки
       .pipe(p.gp.sourcemaps.init()) // Инициализируем gulp-sourcemaps
+      .pipe(p.gp.babel({
+        presets: ['env']
+      })) // Babel
       .pipe(p.gp.concat("app.js")) // Объединяем все JS файлы в один
       .pipe(p.gp.uglify()) // Минифицируем наш js файл
       .pipe(p.gp.sourcemaps.write()) // Добавляем sourcemaps в файл .js
-      .pipe(p.gp.rename({ suffix: ".min" })) // Переименовываем
+      .pipe(p.gp.rename({
+        suffix: ".min"
+      })) // Переименовываем
       .pipe(p.gulp.dest(p.paths.build.js))
-      .pipe(p.browserSync.reload({ stream: true }));
+      .pipe(p.browserSync.reload({
+        stream: true
+      }));
   });
 };
