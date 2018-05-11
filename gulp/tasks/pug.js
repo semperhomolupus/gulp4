5982d64152ff3b14cb44babda80cbab9bf960616
Егор Дьяченко <egordacenko@Egors-Mac-mini.local>
2018-05-11 17:47:26 +0300

    Валидация HTML

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,11 +1,11 @@
-module.exports = function() {
-  p.gulp.task("pug", function() {
+module.exports = function () {
+  p.gulp.task("pug", function () {
     return p.gulp
       .src(p.paths.src.pug)
       .pipe(p.gp.newer(p.paths.build.html)) // Проверяем из кеша
       .pipe(
         p.gp.plumber({
-          errorHandler: p.gp.notify.onError(function(error) {
+          errorHandler: p.gp.notify.onError(function (error) {
             return {
               title: "PUG - ошибка при сборке .pug",
               message: error.message
@@ -13,8 +13,14 @@ module.exports = function() {
           })
         })
       ) // Предотвращает остановку плагина при возникновении ошибки
-      .pipe(p.gp.pug({ pretty: true })) // Конвертируем
+      .pipe(p.gp.pug({
+        pretty: true
+      })) // Конвертируем
       .pipe(p.gulp.dest(p.paths.build.html)) // Копируем
-      .pipe(p.browserSync.reload({ stream: true }));
+      .pipe(p.gp.htmlhint()) // Валидация
+      .pipe(p.gp.htmlhint.failAfterError())
+      .pipe(p.browserSync.reload({
+        stream: true
+      }));
   });
 };
