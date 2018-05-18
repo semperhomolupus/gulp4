64ee208aa1cec0ed781f7da6c34300b788297d23
Егор Дьяченко <e.dyachenko@pugofka.com>
2018-05-18 14:11:01 +0300

    Добавил eslint

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,30 +1,43 @@
-module.exports = function () {
-  p.gulp.task("js", function () {
+module.exports = function() {
+  p.gulp.task('js', function() {
     return p.gulp
       .src(p.paths.src.js)
+      .pipe(p.gp.newer(p.paths.build.js))
       .pipe(
         p.gp.plumber({
-          errorHandler: p.gp.notify.onError(function (error) {
+          errorHandler: p.gp.notify.onError(function(error) {
             return {
-              title: "JavaScript - ошибка при сборке .js",
-              message: error.message
+              title: 'JavaScript - ошибка при сборке .js',
+              message: error.message,
             };
-          })
+          }),
         })
       ) // Предотвращает остановку плагина при возникновении ошибки
       .pipe(p.gp.sourcemaps.init()) // Инициализируем gulp-sourcemaps
-      .pipe(p.gp.babel({
-        presets: ['env']
-      })) // Babel
-      .pipe(p.gp.concat("app.js")) // Объединяем все JS файлы в один
+      .pipe(
+        p.gp.eslint({
+          fix: true,
+        })
+      )
+      .pipe(p.gp.eslint.format())
+      .pipe(
+        p.gp.babel({
+          presets: ['env'],
+        })
+      ) // Babel
+      .pipe(p.gp.concat('app.js')) // Объединяем все JS файлы в один
       .pipe(p.gp.uglify()) // Минифицируем наш js файл
       .pipe(p.gp.sourcemaps.write()) // Добавляем sourcemaps в файл .js
-      .pipe(p.gp.rename({
-        suffix: ".min"
-      })) // Переименовываем
+      .pipe(
+        p.gp.rename({
+          suffix: '.min',
+        })
+      ) // Переименовываем
       .pipe(p.gulp.dest(p.paths.build.js))
-      .pipe(p.browserSync.reload({
-        stream: true
-      }));
+      .pipe(
+        p.browserSync.reload({
+          stream: true,
+        })
+      );
   });
 };
---
+++ src/js/example1.js
@@ -0,0 +1,11 @@
+var hello = '5';
+
+var meow = '5';
+
+var me = hello === meow;
+
+function ohello(e) {
+  console.log(me);
+}
+
+ohello();
