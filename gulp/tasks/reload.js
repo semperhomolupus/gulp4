b6423f1f0a48e52b3d7bb4c8bdeca0f536edc825
Egor Dyachenko <whitebestnik@yandex.ru>
2018-08-22 16:39:33 +0300

    fix reload plugin

--- gulp/tasks/reload.js
+++ gulp/tasks/reload.js
@@ -2,7 +2,7 @@ module.exports = function () {
   p.gulp.task("reload", function () {
     p.browserSync.init({
       server: {
-        baseDir: "./" + p.paths.project
+        baseDir: "./build/tpl"
       }
     });
   });
