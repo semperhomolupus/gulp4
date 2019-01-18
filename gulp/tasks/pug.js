ed508abe5adf65fc2294aed279ef1a6f3caf5fdd
Egor Dyachenko <whitebestnik@yandex.ru>
2019-01-18 14:58:11 +0300

    Убрал минификацию HTML в продакшене

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,36 +1,34 @@
-module.exports = function () {
-  p.gulp.task("pug", function () {
+module.exports = function() {
+  p.gulp.task("pug", function() {
     return p.gulp
       .src(p.paths.src.pug)
       .pipe(p.gp.newer(p.paths.build.html)) // Проверяем из кеша
       .pipe(
         p.gp.plumber({
-          errorHandler: p.gp.notify.onError(function (error) {
+          errorHandler: p.gp.notify.onError(function(error) {
             return {
               title: "PUG - ошибка при сборке .pug",
-              message: error.message,
+              message: error.message
             };
-          }),
+          })
         })
       ) // Предотвращает остановку плагина при возникновении ошибки
       .pipe(
-        develop ?
         p.gp.pug({
-          pretty: true,
-        }) :
-        p.gp.pug()
+          pretty: true
+        })
       ) // Конвертируем
       .pipe(p.gulp.dest(p.paths.build.html)) // Копируем
       .pipe(
         p.gp.htmlhint({
           "attr-lowercase": false,
-          "spec-char-escape": false,
+          "spec-char-escape": false
         })
       ) // Валидация
       .pipe(p.gp.htmlhint.failAfterError())
       .pipe(
         p.browserSync.reload({
-          stream: true,
+          stream: true
         })
       );
   });
